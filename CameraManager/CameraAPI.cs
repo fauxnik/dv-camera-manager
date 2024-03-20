9ab7b584a965a5c28de6a81bb0353c196e9b4982
Niko Fox <niko@fauxnik.me>
2024-03-19 21:11:11 -0700

    Validate objects with useful error messages

--- CameraManager/CameraAPI.cs
+++ CameraManager/CameraAPI.cs
@@ -7,13 +7,24 @@ namespace CameraManager;
 
 public static class CameraAPI
 {
-	public static Camera GetCamera(CameraType type) => type switch
+	public static Camera GetCamera(CameraType type)
 	{
-		CameraType.World => PlayerManager.PlayerCamera,
-		CameraType.UI => new List<Camera>(Camera.allCameras).Find(cam => cam.name == "SecondCamera"),
-		CameraType.Effects => new List<Camera>(Camera.allCameras).Find(cam => cam.name == "ThirdCamera"),
-		_ => throw new ArgumentOutOfRangeException(nameof(type), $"Unexpected camera type: {type}"),
-	};
+		Camera? camera = type switch
+		{
+			CameraType.World => PlayerManager.PlayerCamera,
+			CameraType.UI => new List<Camera>(Camera.allCameras).Find(cam => cam.name == "SecondCamera"),
+			CameraType.Effects => new List<Camera>(Camera.allCameras).Find(cam => cam.name == "ThirdCamera"),
+			_ => throw new ArgumentOutOfRangeException(nameof(type), $"Unexpected camera type: {type}"),
+		};
+
+		if (camera == null)
+		{
+			Main.LogError($"Couldn't find the {type} camera!\nAvailable cameras:\n[\n\t{string.Join<Camera>(",\n\t", Camera.allCameras)}\n]");
+			throw new NullReferenceException($"[CameraAPI] Couldn't find the {type} camera");
+		}
+
+		return camera;
+	}
 
 	public static Camera CloneCamera(Camera source)
 	{
@@ -24,7 +35,13 @@ public static class CameraAPI
 		// The player camera has child objects that we don't need
 		for (int i = gameObject.transform.childCount; i > 0; --i)
 		{
-			Destroy(gameObject.transform.GetChild(i - 1).gameObject);
+			Transform? child = gameObject.transform.GetChild(i - 1);
+			if (child?.gameObject == null)
+			{
+				Main.LogWarning($"Skipping unexpectedly null child game object for {source}.");
+				continue;
+			}
+			Destroy(child.gameObject);
 		}
 
 		return gameObject.GetComponent<Camera>();
